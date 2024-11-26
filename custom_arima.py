0adf6b381281a0703a0c32fb81b2e48aa06b3b3c
Nathan <nathannotaras@gmail.com>
2024-11-27 02:01:17 +1100

    Update custom_arima.py

--- custom_arima.py
+++ custom_arima.py
@@ -94,13 +94,15 @@ def arima(x, p,d,q,h):
     for t in range(1,len(y)):
         yt[t] = y[t-1] +zt1[t]
 
-    fut = np.zeros(h)
     ztf = np.hstack([zt,np.zeros(h)])
     ztf[-h] = ps.dot(np.array([zt[-i] for i in range(1,p+1)])) +th.dot(np.array([e[-i] for i in range(1,q+1)]))
     fut[0] = y[-1]+ztf[-h]
+    
     for i in range(1,h):
-        ztf[-h+i] = ps.dot(np.array([ztf[-i] for i in range(1,p+1)]))
-        fut[i] = fut[i-1]+ztf[i]
+        ar_term = ps.dot(np.array([ztf[-(i+j)] for j in range(1, p+1)])) if p > 0 else 0
+        ztf[len(zt) + i] = ar_term 
+        fut[i] = y[-1] + np.sum(ztf[len(zt):len(zt) + i + 1])
+
     return yt, fut
 result = []
 test = x[-h:]
